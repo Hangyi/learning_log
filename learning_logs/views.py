11da9dbd40360e2eb0968c6268f1b5fbd13855e4
Hangyi <sweat0909@gmail.com>
2018-10-10 19:23:13 +0800

    use 'get_object_or_404' replace 'get'

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -22,7 +22,6 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
-    # topic = Topic.objects.get(id=topic_id)
     topic = get_object_or_404(Topic, id=topic_id)
     # 确认请求的主题属于当前用户
     check_topic_owner(request, topic)
@@ -53,7 +52,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """在特定的主题中添加新条目"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(request, topic)
 
     if request.method != 'POST':
@@ -76,7 +75,8 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
     """编辑既有条目"""
-    entry = Entry.objects.get(id=entry_id)
+    # entry = Entry.objects.get(id=entry_id)
+    entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     check_topic_owner(request, topic)
 
